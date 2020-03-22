2d0ec4e2cd6fb184d4b02e5441844fe1e6df1d40
Kirill Scherbakoff <elkirrs@gmail.com>
2020-03-22 22:51:54 +0300

    feat: expression-calculator

--- src/index.js
+++ src/index.js
@@ -4,9 +4,22 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let arrExpr = expr.replace(/\s/g, "");
+    let countA = 0;
+    let countB = 0;
+    for (let el of arrExpr){
+        if(el === '(') countA++;
+        if(el === ')') countB++;
+    }
+
+    if(countA !== countB) throw new Error('ExpressionError: Brackets must be paired');
+    for (let i = 0; i < arrExpr.length; i++){
+        if (arrExpr[i] === "/" && arrExpr[++i] === "0") throw new Error('TypeError: Division by zero.');
+    }
+    let result = new Function("return " + arrExpr);
+    return result();
 }
 
 module.exports = {
     expressionCalculator
-}
+};
